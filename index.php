0e81aba4f6061778c9f3517a961f50924e13a94c
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-03-01 16:33:32 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html lang="ru">
+  <head>
+    <title><?= $title; red ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+   body {
+    background: #333; 
+    color: #fc0; 
+   }
+  </style>
+  </head>
+  <body>
+      <img src="p.png" width="250" height="250"/>
+    <header class="main-header">
+      <h1 class="visually-hidden">My site</h1>
+    </header>
+    <div class="main-content">
+      <main class="content"><?= $content; ?></main>
+    </div>
+    <footer class="main-footer"> Essa</footer>
+  </body>
+</html>
