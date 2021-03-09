66fb77d8cb5de892ee23dc539be6cae9a5104e03
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-03-09 23:27:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Portfolio</title>
+<style>
+body {
+  background-image: url('https://bootstraptema.ru/_sf/26/51506352.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+a:link {
+  color: green;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: pink;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+	<title>essa</title>
 </head>
-<body>
-	<div><img src="profile.jpg" width="100" height="100" style="vertical-align:right; margin-top:0px;  border-radius: 50%; float: center;" border="2";>
+<body style="background-color:powderblue;">
+	<div><img src="profile.jpg" width="300" height="300" style="vertical-align:right; margin-top:0px;  border-radius: 50%; float: right;" border="2";>
 	</div>
 	<div>
-		<h1>Esenbek</h1>
-     	<h1>Mambetov</h1>
+		<h1 style="color:blue;" ><em> Esenbek</em> </h1>
+     	<h1 style="color:blue;" ><i>Mambetov</i></h1>
 	</div>
 <div>
-	<h3>Social Status: Student</h3>
-	<h3>Age: 17 years</h3>
-  <h4>Phone number: +996-708-601-601 </h4>
-	<p><a href="https://www.instagram.com/esen411k/"> My Instagramm</a></p>
-	<p><a href="https://m.facebook.com/profile.php?id=100016479917100"> FaceBook</a></p>
-	<p><a href="https://github.com/EsenbekM"> GitHub</a></p>
+	<h3 style="color:green;">Social Status: Student</h3>
+	<h3 style="color:orange;">Age: 17 years</h3>
+  <h4 style="color:pink;">Phone number: +996-708-601-601 </h4>
+	<p><a style="color:red;" target="_blank"  href="https://www.instagram.com/esen411k/" title="Follow me :)"> My Instagramm</a></p>
+	<p><a style="color:blue;" target="_blank" href="https://m.facebook.com/profile.php?id=100016479917100" title="It's only for PUBG :|"> FaceBook</a></p>
+	<p><a style="color:yelow;" target="_blank" href="https://github.com/EsenbekM" title="My works"> GitHub</a></p>
+    <p><a style="color:orange;" target="_blank" href="mailto:esenbekm03@gmail.com" title="Send me massage">My email</a></p>
 </div>
     
     
