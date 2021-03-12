e78371a021fd435c4b604bca8e393178dd0b3d79
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-03-12 22:17:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="icon" href="logo.png" type="image/x-icon">
+<link rel="icon" href="https://thumbs.dreamstime.com/b/%D0%BC%D0%B0%D0%BB%D1%8C%D1%87%D0%B8%D0%BA-%D1%81-%D0%BA%D1%80%D1%83%D0%B3%D0%BB%D1%8B%D0%BC-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%BE%D0%BC-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%B0-%D1%81%D1%82%D0%B5%D0%BA%D0%B5%D0%BB-%D0%BC%D0%B8%D0%BD%D0%B8%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC-%D1%81%D1%82%D0%B8%D0%BB%D0%B5%D0%BC-132246818.jpg" type="image/x-icon">
 <style>
 body {
   background-image: url('https://bootstraptema.ru/_sf/26/51506352.jpg');
