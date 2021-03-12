5a757c1e4f3d9c0c74f90cc3ac6720b6f3a52a6e
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-03-12 13:56:45 +0600

    Update index.php

--- index.php
+++ index.php
@@ -46,10 +46,10 @@ a:active {
 	<p><a style="color:blue;" target="_blank" href="https://m.facebook.com/profile.php?id=100016479917100" title="It's only for PUBG :|"> FaceBook</a></p>
 	<p><a style="color:yelow;" target="_blank" href="https://338b3b1bbb6f.ngrok.io" title="My works"> GitHub</a></p>
         <a target="_blank" href="https://www.instagram.com/esen411k/">
-<img src="ins.png" alt="HTML tutorial" style="width:100px;height:100px;">
+<img src="ins.png" alt="HTML tutorial" style="width:50px;height:50px;">
 </a>
 	<a target="_blank" href="https://m.facebook.com/profile.php?id=100016479917100">
-<img src="fb.png" alt="HTML tutorial" style="width:100px;height:100px;">
+<img src="fb.png" alt="HTML tutorial" style="width:50px;height:50px;">
 </a>
 	<p><a style="color:orange;" target="_blank" href="mailto:esenbekm03@gmail.com" title="Send me massage">My email</a></p>
 </div>
