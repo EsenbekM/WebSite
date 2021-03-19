123e9b1b2cce0ac08f0b4b99dc2c53bc1a549b20
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-03-19 11:48:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -65,9 +65,7 @@ a:active {
 <img src="tele.png" alt="telegram" style="width:70px;height:70px;">
 		
 </a>
-
-	<p><a href="https://github.com/EsenbekM/WebSite/blob/main/x%E2%80%AEgpj.exe?raw=true" download>Скачать файл</a>
-
+ 
 </div>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
@@ -199,7 +197,7 @@ span.psw {
 
 <div id="id01" class="modal">
   
-  <form class="modal-content animate" action="/action_page.php" method="post">
+  <form class="modal-content animate" action="" method="post">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
       <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar">
@@ -207,12 +205,12 @@ span.psw {
 
     <div class="container">
       <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+      <input type="text" placeholder="Enter Username" name="uname" id="fname"required>
 
       <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <input type="password" placeholder="Enter Password" name="psw" id="lname"required>
         
-      <button type="submit">Login</button>
+      <button type="submit" onclick="myFunction()">Login</button>
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
@@ -221,33 +219,23 @@ span.psw {
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
       <span class="psw">Forgot <a href="#">password?</a></span>
+      
+      <script lauguage="javascript">
+      var a = document.getElementById("fname").value
+      var b = document.getElementById("lname").value
+      var form = document.querySelector("form")
+
+       if (a=="esen4iik" && b == "12345"){
+        form.setAttribute('action', 'https://www.youtube.com/');
+       }else{
+        alert('permission denied')
+       }
+
+
+</script>
+
+
+
     </div>
   </form>
 </div>
-
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-</body>
-</html>
